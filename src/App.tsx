049bd4bdd30a22ff5555a787c8b7cd618f785cd3
Sabyrka <sabyrkaga@gmail.com>
2026-01-01 15:37:17 +0500

    feat: add track details display and audio controls for selected tracks

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,18 @@ interface TrackAttachment {
   url: string
 }
 
+interface TrackDetailsAttributes {
+  title: string
+  lyrics: string | null
+}
+
 interface TrackListItemAttributes {
   title: string
   attachments: TrackAttachment[]
+}
+
+interface TrackDetailsResource {
+  attributes: TrackDetailsAttributes
 }
 
 interface TrackListItemResource {
@@ -16,6 +25,8 @@ interface TrackListItemResource {
 
 export const App = () => {
   const [selectedTrackId, setSelectedTrackId] = useState<string | null>(null)
+  const [selectedTrack, setSelectedTrack] =
+    useState<TrackDetailsResource | null>(null)
   const [tracks, setTracks] = useState<TrackListItemResource[] | null>(null)
 
   useEffect(() => {
@@ -27,6 +38,19 @@ export const App = () => {
       .then((res) => res.json())
       .then((json) => setTracks(json.data))
   }, [])
+
+  useEffect(() => {
+    fetch(
+      `https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${selectedTrackId}`,
+      {
+        headers: {
+          'api-key': import.meta.env.VITE_API_KEY,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((json) => setSelectedTrack(json.data))
+  }, [selectedTrackId])
 
   if (tracks === null) {
     return (
@@ -49,24 +73,51 @@ export const App = () => {
   return (
     <>
       <h1>MusicFun Player</h1>
-      <button onClick={() => setSelectedTrackId(null)}>Reset selection</button>
-      <ul>
-        {tracks.map((track) => (
-          <li
-            key={track.id}
-            style={{
-              border: `1px solid ${
-                track.id === selectedTrackId ? 'orange' : 'transparent'
-              }`,
-            }}
-          >
-            <div onClick={() => setSelectedTrackId(track.id)}>
-              {track.attributes.title}
+      <button
+        onClick={() => {
+          setSelectedTrackId(null)
+          setSelectedTrack(null)
+        }}
+      >
+        Reset selection
+      </button>
+      <div style={{ display: 'flex', columnGap: '30px' }}>
+        <ul>
+          {tracks.map((track) => (
+            <li
+              key={track.id}
+              style={{
+                border: `1px solid ${
+                  track.id === selectedTrackId ? 'orange' : 'transparent'
+                }`,
+              }}
+            >
+              <div
+                onClick={() => {
+                  setSelectedTrackId(track.id)
+                }}
+              >
+                {track.attributes.title}
+              </div>
+              <audio src={track.attributes.attachments[0].url} controls></audio>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <h2>Track details</h2>
+          {!selectedTrackId ? (
+            'No track selected'
+          ) : (
+            <div>
+              <h3>{selectedTrack?.attributes.title}</h3>
+              <div>
+                <h4>Lyrics</h4>
+                {selectedTrack?.attributes.lyrics || <p>No lyrics</p>}
+              </div>
             </div>
-            <audio src={track.attributes.attachments[0].url} controls></audio>
-          </li>
-        ))}
-      </ul>
+          )}
+        </div>
+      </div>
     </>
   )
 }
