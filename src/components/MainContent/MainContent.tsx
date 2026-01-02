393fc176f4efbaf00c8dcdcbbf0f2bb4d7071e05
Sabyrka <sabyrkaga@gmail.com>
2026-01-02 08:06:16 +0500

    refactor: reorder props in MainContent for improved readability

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -3,17 +3,17 @@ import { Playlist } from '../Playlist'
 import { TrackDetails } from '../TrackDetails'
 
 interface Props {
-  selectedTrackId: string | null
-  setSelectedTrackId: (id: string) => void
   selectedTrack: TrackDetailsResource | null
+  selectedTrackId: string | null
   setSelectedTrack: (track: TrackDetailsResource | null) => void
+  setSelectedTrackId: (id: string) => void
 }
 
 export const MainContent = ({
-  selectedTrackId,
-  setSelectedTrackId,
   selectedTrack,
+  selectedTrackId,
   setSelectedTrack,
+  setSelectedTrackId,
 }: Props) => {
   return (
     <main style={{ display: 'flex', columnGap: '30px' }}>
